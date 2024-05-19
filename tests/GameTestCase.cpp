0425ff9446e28698b68ea7313f93e503b4889a4c
Антон Маланьин <malanin.ao@phystech.edu>
2024-05-19 18:52:23 +0300

    add more game tests

--- tests/GameTestCase.cpp
+++ tests/GameTestCase.cpp
@@ -4,10 +4,27 @@
 #include "Player.h"
 #include "Field.h"
 
-class GameTestCase : public ::testing::Test {};
+class GameTestCase : public ::testing::Test {
+ public:
+  static std::string name;
+  static Player p1;
+  static Player p2;
+  static std::vector<Player> p;
+};
 
-TEST(GameTestCase, AddBotPlayer) {
-  std::string name = "";
-  Player p(name, Field(), true);
-  EXPECT_EQ(p, add_player(1, true));
+std::string GameTestCase::name = "";
+Player GameTestCase::p1(name, Field(), true);
+Player GameTestCase::p2(name, Field(), true);
+std::vector<Player> GameTestCase::p = {p1, p2};
+
+TEST_F(GameTestCase, AddBotPlayer) {
+  EXPECT_EQ(p1, add_player(1, true));
+}
+
+TEST_F(GameTestCase, InstallShips) {
+  EXPECT_NO_THROW(install_ships(p));
+}
+
+TEST_F(GameTestCase, MakeShot) {
+  EXPECT_NO_THROW(make_shot(p, 0));
 }

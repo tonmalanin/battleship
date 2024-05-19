3959b767a51e241f641a64554a07cbef2a09cbde
Антон Маланьин <malanin.ao@phystech.edu>
2024-05-19 19:19:07 +0300

    add more io tests

--- tests/IOTestCase.cpp
+++ tests/IOTestCase.cpp
@@ -1,9 +1,23 @@
-#include "gtest/gtest.h"
-
 #include "IOManager.h"
+#include "gtest/gtest.h"
 
 class IOTestCase : public ::testing::Test {};
 
-TEST(IOTestCase, StartMessage) {
-  EXPECT_NO_THROW(start_message());
+TEST(IOTestCase, StartMessage) { EXPECT_NO_THROW(start_message()); }
+
+TEST(IOTestCase, FinishMessage) {
+  std::string name = "";
+  EXPECT_NO_THROW(finish_message(name));
+}
+
+TEST(IOTestCase, PlaceNotice) {
+  std::string name = "";
+  PlaceNotice note;
+  EXPECT_NO_THROW(notify(&note, name));
+}
+
+TEST(IOTestCase, MoveNotice) {
+  std::string name = "";
+  MoveNotice note;
+  EXPECT_NO_THROW(notify(&note, name));
 }
